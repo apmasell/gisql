3d1797adac303858f5af04db1991711120edf187
Andre Masella <andre@kuum.wlu.ca>
2009-09-28 17:54:08 -0400

    Added count metric

---
+++ src/ca/wlu/gisql/interactome/metrics/Totals.java
@@ -0,0 +1,42 @@
+package ca.wlu.gisql.interactome.metrics;
+
+import java.util.Set;
+
+import ca.wlu.gisql.GisQL;
+import ca.wlu.gisql.interactome.Interactome;
+import ca.wlu.gisql.util.ShowablePrintWriter;
+
+public class Totals implements Metrics {
+
+	private int genes = 0;
+
+	private int interactions = 0;
+
+	public void countGene(double membership) {
+		if (GisQL.isPresent(membership))
+			genes++;
+	}
+
+	public void countInteraction(double membership) {
+		if (GisQL.isPresent(membership))
+			interactions++;
+	}
+
+	public int getGeneCount() {
+		return genes;
+	}
+
+	public int getInteractionCount() {
+		return interactions;
+	}
+
+	public void show(ShowablePrintWriter<Set<Interactome>> print) {
+		print.print("# Gene count: ");
+		print.print(genes);
+		print.println();
+		print.print("# Interaction count: ");
+		print.print(interactions);
+		print.println();
+	}
+
+}
