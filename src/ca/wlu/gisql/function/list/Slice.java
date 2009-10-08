99a690a5773215ac3b1c5926ba80a60c68ccb744
Andre Masella <andre@kuum.wlu.ca>
2009-10-08 13:46:21 -0400

    Fixed order and type or parameters in slice

--- src/ca/wlu/gisql/function/list/Slice.java
+++ src/ca/wlu/gisql/function/list/Slice.java
@@ -3,10 +3,14 @@ package ca.wlu.gisql.function.list;
 import java.util.List;
 
 import ca.wlu.gisql.ast.Function;
+import ca.wlu.gisql.ast.type.ListType;
 import ca.wlu.gisql.ast.type.Type;
+import ca.wlu.gisql.ast.type.TypeVariable;
 import ca.wlu.gisql.vm.Machine;
 
 public class Slice extends Function {
+
+	private static final Type list = new ListType(new TypeVariable());
 
 	public static final Function self = new Slice();
 
@@ -14,8 +18,7 @@ public class Slice extends Function {
 		super(
 				"slice",
 				"Takes part of a list. If the end index is negative, the counting is done from the end of the list.",
-				Type.InteractomeType, Type.NumberType, Type.NumberType,
-				Type.InteractomeType);
+				Type.NumberType, Type.NumberType, list, list);
 	}
 
 	@Override
@@ -27,8 +30,8 @@ public class Slice extends Function {
 		 * inclusive on the start and exclusive on the end. Negative indicies on
 		 * the end.
 		 */
-		int start = (Integer) parameters[0];
-		int end = (Integer) parameters[1];
+		int start = ((Long) parameters[0]).intValue();
+		int end = ((Long) parameters[1]).intValue();
 		List list = (List) parameters[2];
 
 		/* Fix 0-base. */
