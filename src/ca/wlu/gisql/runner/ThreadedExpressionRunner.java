fa20185774e497e2e68ead03fdea26447bcfb0d4
Andre Masella <andre@kuum.wlu.ca>
2009-10-09 15:28:32 -0400

    Added background-threaded version of ExpressionRunner

---
+++ src/ca/wlu/gisql/runner/ThreadedExpressionRunner.java
@@ -0,0 +1,128 @@
+package ca.wlu.gisql.runner;
+
+import java.io.File;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import org.apache.log4j.Logger;
+
+import ca.wlu.gisql.ast.type.Type;
+import ca.wlu.gisql.environment.UserEnvironment;
+
+public class ThreadedExpressionRunner extends ExpressionRunner {
+
+	private abstract class Action {
+		abstract void perform();
+	}
+
+	private class RunFile extends Action {
+		final File file;
+		final Type type;
+
+		public RunFile(File file, Type type) {
+			super();
+			this.file = file;
+			this.type = type;
+		}
+
+		@Override
+		void perform() {
+			run(file, type);
+		}
+	}
+
+	private class Runner implements Runnable {
+
+		public void run() {
+			while (true) {
+				try {
+					Action action = todo.take();
+					if (action == null) {
+						return;
+					}
+
+					action.perform();
+				} catch (InterruptedException e) {
+					log.error("Interruption while reading from queue", e);
+					return;
+				}
+			}
+		}
+	}
+
+	private class RunString extends Action {
+		final String line;
+		final Type type;
+
+		public RunString(String line, Type type) {
+			super();
+			this.line = line;
+			this.type = type;
+		}
+
+		@Override
+		void perform() {
+			run(line, type);
+		}
+	}
+
+	private static final Logger log = Logger
+			.getLogger(ThreadedExpressionRunner.class);
+
+	private boolean busy = false;
+
+	private final BlockingQueue<Action> todo = new ArrayBlockingQueue<Action>(
+			100);
+
+	private final Thread worker;
+
+	public ThreadedExpressionRunner(UserEnvironment environment,
+			ExpressionRunListener listener) {
+		super(environment, listener);
+		worker = new Thread(new Runner());
+		worker.start();
+	}
+
+	public boolean isBusy() {
+		return busy;
+	}
+
+	@Override
+	public boolean run(File file, Type type) {
+		if (Thread.currentThread() == worker) {
+			busy = true;
+			boolean result = super.run(file, type);
+			busy = false;
+			return result;
+		} else {
+			try {
+				todo.put(new RunFile(file, type));
+				return true;
+			} catch (InterruptedException e) {
+				log.error("Could not send run(file) command.", e);
+				e.printStackTrace();
+				return false;
+			}
+		}
+	}
+
+	@Override
+	public boolean run(String command, Type type) {
+		if (Thread.currentThread() == worker) {
+			busy = true;
+			boolean result = super.run(command, type);
+			busy = false;
+			return result;
+		} else {
+			try {
+				todo.put(new RunString(command, type));
+				return true;
+			} catch (InterruptedException e) {
+				log.error("Could not send run(string) command.", e);
+				e.printStackTrace();
+				return false;
+			}
+		}
+	}
+
+}
