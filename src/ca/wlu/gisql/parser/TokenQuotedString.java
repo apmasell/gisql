efd897e8c4abc9a872659e50c69ee904be74678d
Andre Masella <andre@kuum.wlu.ca>
2009-11-05 18:33:52 -0500

    Added better esacping support for string literals

--- src/ca/wlu/gisql/parser/TokenQuotedString.java
+++ src/ca/wlu/gisql/parser/TokenQuotedString.java
@@ -38,8 +38,66 @@ public class TokenQuotedString extends Token {
 			} else if (codepoint == '\\') {
 				parser.position++;
 				if (parser.position < parser.input.length()) {
-					sb.append(parser.input.charAt(parser.position));
+					char c = parser.input.charAt(parser.position);
 					parser.position++;
+
+					switch (c) {
+					case 'a':
+						sb.append('\u0007');
+						break;
+
+					case 'b':
+						sb.append('\b');
+						break;
+
+					case 'f':
+						sb.append('\f');
+						break;
+
+					case 'n':
+						sb.append('\n');
+						break;
+
+					case 'r':
+						sb.append('\r');
+						break;
+
+					case 't':
+						sb.append('\t');
+						break;
+
+					case 'u':
+						Integer result = pullHex(parser, 4);
+						if (result == null) {
+							return false;
+						}
+						sb.appendCodePoint(result);
+						break;
+
+					case 'U':
+						result = pullHex(parser, 8);
+						if (result == null) {
+							return false;
+						}
+						sb.appendCodePoint(result);
+						break;
+
+					case 'v':
+						sb.append('\u000B');
+						break;
+
+					case 'x':
+						result = pullHex(parser, 2);
+						if (result == null) {
+							return false;
+						}
+						sb.appendCodePoint(result);
+						break;
+
+					default:
+						sb.append(c);
+						break;
+					}
 				} else {
 					success = false;
 					break;
@@ -64,4 +122,22 @@ public class TokenQuotedString extends Token {
 		return true;
 	}
 
+	private Integer pullHex(Parser parser, int digits) {
+		int result = 0;
+		while (digits > 0 && parser.position < parser.input.length()) {
+			int value = Character.digit(parser.input.charAt(parser.position),
+					16);
+			if (value == -1) {
+				return null;
+			}
+			result = result * 0x10 + value;
+			parser.position++;
+			digits--;
+		}
+		if (digits == 0 && result != 0) {
+			return result;
+		} else {
+			return null;
+		}
+	}
 }
