a52ca982a8ff8d60ec110788bc8ca4b4c5f8d536
Andre Masella <andre@kuum.wlu.ca>
2010-10-07 12:12:53 -0400

    Make "do" polymorphic

--- src/ca/wlu/gisql/environment/functions/SilentFunction.java
+++ src/ca/wlu/gisql/environment/functions/SilentFunction.java
@@ -1,6 +1,7 @@
 package ca.wlu.gisql.environment.functions;
 
 import ca.wlu.gisql.ast.type.Type;
+import ca.wlu.gisql.ast.type.TypeVariable;
 import ca.wlu.gisql.ast.type.Unit;
 import ca.wlu.gisql.ast.util.Function;
 import ca.wlu.gisql.interactome.CachedInteractome;
@@ -12,14 +13,16 @@ public final class SilentFunction extends Function {
 
 	public SilentFunction(ExpressionRunner runner) {
 		super(runner, "do", "Runs a query and supresses the results",
-				Type.InteractomeType, Type.UnitType);
+				new TypeVariable(), Type.UnitType);
 	}
 
 	@Override
 	public Object run(Object... parameters) {
-		ProcessableInteractome processableInteractome = CachedInteractome.wrap(
-				(Interactome) parameters[0], null);
-		processableInteractome.process();
+		if (parameters[0] instanceof Interactome) {
+			ProcessableInteractome processableInteractome = CachedInteractome
+					.wrap((Interactome) parameters[0], null);
+			processableInteractome.process();
+		}
 		return Unit.nil;
 	}
 
